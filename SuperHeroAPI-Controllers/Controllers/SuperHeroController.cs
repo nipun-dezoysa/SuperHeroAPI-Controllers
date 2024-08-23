afe304d40cc4aa42fa3f050119907edc326c7072
nipun-dezoysa <nipunavishka123@gmail.com>
2024-08-23 22:53:08 +0530

    basic get method

---
+++ SuperHeroAPI-Controllers/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_Controllers.Entities;
+
+namespace SuperHeroAPI_Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Superman",
+                    FirstName = "Clark",
+                    LastName = "Kent",
+                    Place = "Metropolis",
+                }
+            };
+            return Ok(heroes);
+        }
+    }
+}
