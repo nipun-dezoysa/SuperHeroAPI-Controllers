12be384feb9133a245645809ec5a2477e0e5f553
nipun-dezoysa <nipunavishka123@gmail.com>
2024-08-25 10:25:57 +0530

    get hero

--- SuperHeroAPI-Controllers/Controllers/SuperHeroController.cs
+++ SuperHeroAPI-Controllers/Controllers/SuperHeroController.cs
@@ -22,5 +22,16 @@ namespace SuperHeroAPI_Controllers.Controllers
             var heroes = await _context.SuperHeroes.ToListAsync();
             return Ok(heroes);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+            {
+                return NotFound("Hero not found.");
+            }
+            return Ok(hero);
+        }
     }
 }
